f16f479e52220605c24b49ec797ae1999a8e2534
Nick Moore <nick@pilotmoon.com>
2014-11-28 09:41:58 +0000

    Initial Commit

---
+++ WritLarge/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WritLarge
+//
+//  Created by Nicholas Moore on 28/11/2014.
+//  Copyright (c) 2014 Pilotmoon Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ WritLarge/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  WritLarge
+//
+//  Created by Nicholas Moore on 28/11/2014.
+//  Copyright (c) 2014 Pilotmoon Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
